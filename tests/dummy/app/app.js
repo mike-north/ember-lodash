d53a821e1dab99e6c5862351e569020c41d5ec93
Mike North <michael.l.north@gmail.com>
2015-07-05 13:45:04 -0700

    initial commit

---
+++ tests/dummy/app/app.js
@@ -0,0 +1,21 @@
+import Ember from 'ember';
+import Resolver from 'ember/resolver';
+import loadInitializers from 'ember/load-initializers';
+import config from './config/environment';
+import { string } from 'lodash';
+
+var App;
+
+console.log('string',string);
+
+Ember.MODEL_FACTORY_INJECTIONS = true;
+
+App = Ember.Application.extend({
+  modulePrefix: config.modulePrefix,
+  podModulePrefix: config.podModulePrefix,
+  Resolver: Resolver
+});
+
+loadInitializers(App, config.modulePrefix);
+
+export default App;
