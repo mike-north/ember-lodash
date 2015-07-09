4f5536094792d0f9b05fd3b489fafcc7da4240c9
Jason Mitchell <jason.mitchell.w@gmail.com>
2015-07-08 22:38:54 -0700

    removing the need to maintain lodash fork

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 /* jshint node: true */
 'use strict';
 
+var path = require('path');
+var mergeTrees = require('broccoli-merge-trees');
+
 module.exports = {
-  name: 'lodash'
+  name: 'lodash',
+
+  treeForAddon: function(tree) {
+    var lodashPath = path.join(this.project.root, 'node_modules', 'ember-lodash', 'node_modules', 'lodash-es');
+
+    if (this.app.name === 'dummy') {
+      lodashPath = path.join(this.project.root, 'node_modules', 'lodash-es');
+    }
+
+    var lodashTree = this.treeGenerator(lodashPath);
+
+    var trees = mergeTrees([lodashTree, tree], {
+      overwrite: true
+    });
+
+    return this._super.treeForAddon.call(this, trees);
+  },
 };
